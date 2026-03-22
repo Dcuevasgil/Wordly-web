3162b6bb7cb0a5808e747a2ffd38cfd81e3b65f9
Dcuevasgil <dcuevasgil@gmail.com>
2026-03-22 02:58:03 +0100

    feat(auth): implement base login UI structure and styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './Inicio.jsx'
+import App from './modules/auth/pages/LoginPage.jsx'
 import './styles/main.css'
+
+function main() {
+  console.log("Aplicacion lista");
+}
+
+main(); // 👈 LLAMADA AQUÍ
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
